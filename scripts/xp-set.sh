8f41da5957c86ad418ea0745fecef23911169f05
Ben Paige <benpaigeiii@hotmail.com>
2015-06-19 18:18:47 -0400

    first attempt in added 'add' functionality

--- scripts/xp-set.sh
+++ scripts/xp-set.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
 CURRENT_PATH=`pwd`
-BASE_PATH=`basename $CURRENT_PATH`
+BASE_PATH=`basename "$CURRENT_PATH"`
+NEW_PATH=""
 
 OIFS=$IFS
-IFS='|'
-FIELD_SET=($BASE_PATH)
 IFS=':'
 FIELD=($1)
 FIELD_TYPE=${FIELD[0]}
 FIELD_VALUE=${FIELD[1]}
 
-echo "[FIELDS] type: ${FIELD_TYPE}; value: ${FIELD_VALUE}"
+if [[ ! $BASE_PATH =~ $.*FIELD_TYPE:.* ]]
+then # Add new field if none present
+	NEW_PATH=$BASE_PATH|$1
+else # Find field and update
+	IFS='|'
+	FIELD_SET=($BASE_PATH)
+
+	echo "[FIELDS] type: ${FIELD_TYPE}; value: ${FIELD_VALUE}"
+
+
+	for field in ${FIELD_SET[@]}
+	do
+		if [[ ${#NEW_PATH}>0 ]]
+		then
+			NEW_PATH+="|"
+		fi
+
+		if [[ $field =~ $FIELD_TYPE:.* ]]
+		then
+			NEW_PATH+=$1
+		else
+			NEW_PATH+=$field
+		fi
+	done
+fi
 
 IFS=$OIFS
 
-NEW_PATH=""
-for field in ${FIELD_SET[@]}
-do
-	if [[ ${#NEW_PATH}>0 ]]
-	then
-		NEW_PATH+="|"
-	fi
-
-	if [[ $field =~ $FIELD_TYPE:.* ]]
-	then
-		NEW_PATH+=$1
-	else
-		NEW_PATH+=$field
-	fi
-done
 echo new path: $NEW_PATH
 
 cd ..
