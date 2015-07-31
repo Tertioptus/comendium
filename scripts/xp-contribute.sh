9edae812007bf74461236af988f91a995c3d28f0
Ben Paige <benpaigeiii@hotmail.com>
2015-07-31 15:45:16 -0400

    quoute qualify contribution parameters

--- scripts/xp-contribute.sh
+++ scripts/xp-contribute.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir contribution:$1
+mkdir "contribution:$1"
 
-cd contribution:$1
+cd "contribution:$1"
 
 if [[ ! -z $2 ]]
 then
- mkdir achievement:$2
+ mkdir "achievement:$2"
 fi
 
 cd ..
