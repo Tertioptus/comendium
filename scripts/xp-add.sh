b3f099aad08bff2a6b6baabe49c844dea55adb88
Ben <benpaigeiii@hotmail.com>
2015-08-01 20:21:16 -0400

    first add completeion

--- scripts/xp-add.sh
+++ scripts/xp-add.sh
@@ -1,7 +1,19 @@
-#1/bin/bash
+#!/bin/bash
 
 #find compendium root
 
 #append backlog
 
-#mkdir s:$1|i:$ID|c:$3|u:$4|a:$5|r:$6
+read -p "Status: u: " STATUS  
+read -p " Points: 00: " POINTS
+read -p "Id: " ID
+read -p "Context: " CONTEXT
+read -p "User: " USER
+read -p "Action: " ACTION
+read -p "Resource:" RESOURCE
+
+STORY_PATH="s:$STATUS|p:$POINTS|i:$ID|c:$CONTEXT|u:$USER|a:$ACTION|r:$RESOURCE"
+mkdir $STORY_PATH
+cd $STORY_PATH
+
+touch tag.story
