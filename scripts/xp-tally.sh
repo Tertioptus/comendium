cf6ba395ad7c0370f5f6a2834c30f8c8334ef328
Ben <benpaigeiii@hotmail.com>
2015-08-10 06:32:55 -0400

    starting tally

--- scripts/xp-tally.sh
+++ scripts/xp-tally.sh
@@ -3,17 +3,27 @@
 
 if [[ `pwd` =~ c:feature ]]
 then
-  LEVEL=99 - feature points
-
+	POINTS_DIRS=()
+	POINTS_DIRS+=(`ls points.*`)
+	if [[ ! -z POINTS_DIRS ]]
+	then
+		POINTS=${POINTS_DIRS[0]:2}
+  		LEVEL=99-$POINTS 
+	else
+	fi
+<<COMMENT1
 else
 	if dependets
 	then
 		LEVEL= lowest level among dependents - number of dependents
 
 	else	#Orphan the story
-    	xp-set s:0
+    	#xp-set s:0
     	LEVEL=00
 	fi
+COMMENT1
 fi
 
-xp-set l:$LEVEL
+echo `pwd`
+echo $LEVEL
+#xp-set l:$LEVEL
