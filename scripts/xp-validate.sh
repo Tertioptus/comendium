da662d35ea4c37f62bce415276465540e4816b59
Ben Paige <benpaigeiii@hotmail.com>
2015-09-09 16:37:25 -0400

    started validate

---
+++ scripts/xp-validate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Validates full compendium structure
+#
+# suggested use is to validate on every commit
+# and add the validation status into the commit log
+#################################################
